e5e6163574ea37461825a201b1cf79c8180de75d
mr19331 <mr19331@gmail.com>
2020-07-15 18:10:32 +0000

    testing calculator

---
+++ scripts/calc.js
@@ -0,0 +1,8 @@
+function addition(numOne, numTwo){
+    if (typeof(numOne) =="number" && typeof(numTwo)=="number"){
+     return numOne + numTwo;   
+    } else{
+        return "Error!";
+    }
+  
+} 
